b3a7e1104ec766d8dbcef56cb9c9982faa4badf9
adburke <adburke@fullsail.edu>
2013-05-16 19:05:57 -0500

    IBAction function for buttons

--- testApp/testApp/ViewController.m
+++ testApp/testApp/ViewController.m
@@ -30,6 +30,106 @@
     }
 }
 
+-(IBAction)onClickBtn:(id)sender
+{
+    
+    UIButton *button = (UIButton*)sender;
+    if (button) {
+        if (button.tag == 0) {
+            NSLog(@"Weight Pressed");
+            
+            if (![workoutType isEqualToString: @"Weight"]) {
+                stepper.value = 1;
+                currentValue = stepper.value;
+            }
+            
+            textField.text = [NSString stringWithFormat:@"%d Weight Workout", currentValue];
+            workoutType = @"Weight";
+            
+            //Self
+            weightButton.enabled = FALSE;
+            weightButton.alpha = 0.5f;
+            //Others
+            swimButton.enabled = TRUE;
+            swimButton.alpha = 1.0f;
+            runButton.enabled = TRUE;
+            runButton.alpha = 1.0f;
+            
+        } else if (button.tag == 1) {
+            NSLog(@"Run Pressed");
+            
+            if (![workoutType isEqualToString: @"Run"]) {
+                stepper.value = 1;
+                currentValue = stepper.value;
+            }
+            
+            textField.text = [NSString stringWithFormat:@"%d Run Workout", currentValue];
+            workoutType = @"Run";
+            
+            //Self
+            runButton.enabled = FALSE;
+            runButton.alpha = 0.5f;
+            //Others
+            weightButton.enabled = TRUE;
+            weightButton.alpha = 1.0f;
+            swimButton.enabled = TRUE;
+            swimButton.alpha = 1.0f;
+            
+        } else if (button.tag == 2) {
+            NSLog(@"Swim Pressed");
+            
+            if (![workoutType isEqualToString: @"Swim"]) {
+                stepper.value = 1;
+                currentValue = stepper.value;
+            }
+            
+            textField.text = [NSString stringWithFormat:@"%d Swim Workout", currentValue];
+            workoutType = @"Swim";
+            
+            //Self
+            swimButton.enabled = FALSE;
+            swimButton.alpha = 0.5f;
+            //Others
+            weightButton.enabled = TRUE;
+            weightButton.alpha = 1.0f;
+            runButton.enabled = TRUE;
+            runButton.alpha = 1.0f;
+            
+        } else if (button.tag == 3) {
+            NSLog(@"Info Pressed");
+            InfoView *infoView = [[InfoView alloc] initWithNibName:@"InfoView" bundle:nil];
+            if (infoView) {
+                [self presentViewController:infoView animated:TRUE completion:nil];
+            }
+        } else if (button.tag == 4) {
+            if ([workoutType isEqualToString:@"Weight"]) {
+                weightWorkout *weight = (weightWorkout*)[workoutFactory createNewWorkout:WEIGHT];
+                if (currentValue > 1) {
+                    textField.text = [NSString stringWithFormat:@"%d Weight Workouts --> %i", currentValue, [weight calculateCaloriesBurned]*currentValue ];
+                } else {
+                    textField.text = [NSString stringWithFormat:@"%d Weight Workout --> %i", currentValue, [weight calculateCaloriesBurned]*currentValue ];
+                }
+            } else if ([workoutType isEqualToString:@"Run"]) {
+                runWorkout *run = (runWorkout*)[workoutFactory createNewWorkout:RUN];
+                if (currentValue > 1) {
+                    textField.text = [NSString stringWithFormat:@"%d Run Workouts --> %i", currentValue, ([run calculateCaloriesBurned]*currentValue) ];
+                } else {
+                    textField.text = [NSString stringWithFormat:@"%d Run Workout --> %i", currentValue, ([run calculateCaloriesBurned]*currentValue) ];
+                }
+            } else if ([workoutType isEqualToString:@"Swim"]) {
+                swimWorkout *swim = (swimWorkout*)[workoutFactory createNewWorkout:SWIM];
+                if (currentValue > 1) {
+                    textField.text = [NSString stringWithFormat:@"%d Swim Workouts --> %i", currentValue, ([swim calculateCaloriesBurned]*currentValue) ];
+                } else {
+                    textField.text = [NSString stringWithFormat:@"%d Swim Workout --> %i", currentValue, ([swim calculateCaloriesBurned]*currentValue) ];
+                }
+            }
+            
+        }
+    }
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
