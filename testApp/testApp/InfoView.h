87a11fae2ee39f1d1cfcbc0da82e45a1a28abcec
adburke <adburke@fullsail.edu>
2013-05-16 19:09:05 -0500

    Setup second modal view

---
+++ testApp/testApp/InfoView.h
@@ -0,0 +1,13 @@
+//
+//  InfoView.h
+//  testApp
+//
+//  Created by Aaron Burke on 5/16/13.
+//  Copyright (c) 2013 Aaron Burke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface InfoView : ViewController
+
+@end
---
+++ testApp/testApp/InfoView.m
@@ -0,0 +1,38 @@
+//
+//  InfoView.m
+//  testApp
+//
+//  Created by Aaron Burke on 5/16/13.
+//  Copyright (c) 2013 Aaron Burke. All rights reserved.
+//
+
+#import "InfoView.h"
+
+@interface InfoView ()
+
+@end
+
+@implementation InfoView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
