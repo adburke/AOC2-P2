3009a4c45b2db64c3c18975fbc7f24c48af8c96c
adburke <adburke@fullsail.edu>
2013-05-16 19:03:17 -0500

    Setup IBOutlets and IBActions for viewconroller

--- testApp/testApp/ViewController.h
+++ testApp/testApp/ViewController.h
@@ -9,5 +9,20 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    IBOutlet UITextField *textField;
+    IBOutlet UIButton *weightButton;
+    IBOutlet UIButton *runButton;
+    IBOutlet UIButton *swimButton;
+    IBOutlet UIButton *calcButton;
+    IBOutlet UIStepper *stepper;
+    
+    int currentValue;
+    NSString *workoutType;
+    
+}
+-(IBAction)onClickIncr:(id)sender;
+-(IBAction)onClickBtn:(id)sender;
+-(IBAction)onChange:(id)sender;
 
 @end
